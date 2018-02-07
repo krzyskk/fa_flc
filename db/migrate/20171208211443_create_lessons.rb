195312e0d529fcab83ca39660ef2e3aed6d63f3d
Krzysiek <kkuchna@gmail.com>
2018-02-07 17:15:07 +0100

    Refactor lessons controller

--- db/migrate/20171208211443_create_lessons.rb
+++ db/migrate/20171208211443_create_lessons.rb
@@ -2,9 +2,6 @@ class CreateLessons < ActiveRecord::Migration[5.1]
   def change
     create_table :lessons do |t|
       t.belongs_to :deck, foreign_key: true
-      t.integer :correct_answers, default: 0, null: false
-      t.integer :wrong_answers, default: 0, null: false
-      t.integer :empty_answers, default: 0, null: false
       t.timestamps
     end
   end
