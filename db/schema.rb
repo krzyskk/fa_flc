02fe7aaaec76af570b6f08195ca5cee9545ccbb7
Krzysiek <kkuchna@gmail.com>
2018-04-12 15:17:28 +0200

    Change sign in window appearance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313215153) do
+ActiveRecord::Schema.define(version: 2018_03_13_215153) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "lesson_id"
