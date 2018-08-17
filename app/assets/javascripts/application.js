b9777e244a70ef1b7135770cc64a68c532df1757
Krzysztof Kuchna <kkuchna@gmail.com>
2018-08-17 23:28:17 +0200

    Add toggler to navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
