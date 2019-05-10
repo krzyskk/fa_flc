22ccb1ea1e3234d979c974b195183a50c5bde8a9
Krzysztof Kuchna <kkuchna@gmail.com>
2019-05-10 23:51:51 +0200

    Modify card package

--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,11 @@ ActiveRecord::Schema.define(version: 2019_03_23_215327) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "timers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
