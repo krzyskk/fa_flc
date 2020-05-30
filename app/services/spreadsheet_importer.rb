b12d143f880e1c9ece0c0b21122c843b76efdfc1
Krzysztof Kuchna <kkuchna@gmail.com>
2020-05-30 12:42:00 +0200

    Add circle ci and docker config

--- app/services/spreadsheet_importer.rb
+++ app/services/spreadsheet_importer.rb
@@ -1,26 +1,21 @@
 # frozen_string_literal: true
 
 class SpreadsheetImporter
-  def initialize
+  def initialize(user)
     require 'google_drive'
     require 'googleauth'
-    session = GoogleDrive::Session.from_config({
-      client_id: Rails.application.credentials.client_id,
-      client_secret: Rails.application.credentials.client_secret,
-    }.to_json)
+    # session = GoogleDrive::Session.from_config({
+    #   client_id: Rails.application.credentials.client_id,
+    #   client_secret: Rails.application.credentials.client_secret,
+    # }.to_json)
+    session = GoogleDrive.login_with_oauth(user.access_token)
+
 
     spreadsheet = session.spreadsheet_by_title('arkusz_testowy')
     @worksheet = spreadsheet.worksheets[0]
   end
 
-  def call(tasks)
-    @worksheet.insert_rows(2, [%w[Name Description Start End Location Summary]])
-    tasks.each do |task|
-      @worksheet.insert_rows(
-        2,
-        [[task.name, task.description, task.start, task.end, task.location, task.summary]],
-      )
-    end
-    @worksheet.save
+  def call(cards)
+    @worksheet
   end
 end
